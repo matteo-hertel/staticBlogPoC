a378091ea79fce52b855e0efd99e992c65283330
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-01-10 10:45:52 +0000

    Deploy matteo-hertel/staticBlogPoC to github.com/matteo-hertel/staticBlogPoC.git:gh-pages

---
+++ path---happy-new-year-780ae25764c6494385b0.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xa1baf37db48d],{300:function(a,e){a.exports={data:{markdownRemark:{html:"<h1>Happy new Year</h1>\n<p>2018 is here 🎉🎉🎉🎉🎉🎉</p>",frontmatter:{title:"Happy New Year",date:"January, 05, 2018",path:"/happy-new-year",tags:["holiday","new-year"],excerpt:"Happy new year 🎉🎉"}}},pathContext:{}}}});
+//# sourceMappingURL=path---happy-new-year-780ae25764c6494385b0.js.map
---
+++ path---index-79ef9f1037e73f96dcbd.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x81b8806e4260],{301:function(a,e){a.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{id:"/home/travis/build/matteo-hertel/staticBlogPoC/src/2018-01-05/index.md absPath of file >>> MarkdownRemark",frontmatter:{title:"Happy New Year",date:"January 05, 2018",path:"/happy-new-year",tags:["holiday","new-year"],excerpt:"Happy new year 🎉🎉"}}}]}},pathContext:{}}}});
+//# sourceMappingURL=path---index-79ef9f1037e73f96dcbd.js.map
